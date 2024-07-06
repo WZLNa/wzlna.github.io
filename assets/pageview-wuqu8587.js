2e53e652e558fdd08f3657640fb2bbe077dc2dc3
WZLNa <117418898+WZLNa@users.noreply.github.com>
2024-07-06 22:25:58 +0800

    Delete assets directory

--- assets/pageview-wuqu8587.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as r}from"./app-gBIhIU6G.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://waline-comment.vuejs.press"};const n=async()=>{try{const{pageviewCount:e}=await r(()=>import("./app-gBIhIU6G.js").then(t=>t.L),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!")}};export{n as updatePageview};
